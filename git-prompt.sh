a4686cdde72dc86f972157e3fc894347d0e9fd06
Jason Hollis <jhollis@jdubb.net>
2015-05-15 10:08:04 -0400

    updated logic for git-prompt

--- git-prompt.sh
+++ git-prompt.sh
@@ -3,12 +3,12 @@
 
 if [ -d ".git" ]; then
     branch="`git branch -a | awk '/\*/ {print $2 }'`"
-    if [ `git status | grep -ic 'untracked files\:'` = 1 ] || [ `git status | grep -ic 'no changes'` = 1 ] || [ `git status | grep -ic '^changes not'` = 1 ]; then 
-        echo "("$branch" $(tput setaf 1)✗$(tput sgr0))"
-    else if [ `git status | grep -ic '^changes to be'` = 1 ]; then
+    if [ `git status | grep -ic 'clean'` = 1 ]; then
+        echo "("$branch" $(tput setaf 2)✔$(tput sgr0))"
+    else if [ `git status | grep -ic 'unstage'` = 1 ]; then 
         echo "("$branch" $(tput setaf 3)⬖$(tput sgr0))"
         else
-            echo "("$branch" $(tput setaf 2)✔$(tput sgr0))"
+            echo "("$branch" $(tput setaf 1)✗$(tput sgr0))"
         fi
     fi
 fi
