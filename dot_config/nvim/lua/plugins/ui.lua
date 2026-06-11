3045895ca76440ae611bd3c4c248c8db7effbc1e
Jason Hollis <400979+CodeBleu@users.noreply.github.com>
2026-06-11 18:40:39 -0400

    Added colorizer plugin in neovim

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -69,5 +69,25 @@ return {
         end,
     },
 
+    -- Show colors in files
+    {
+      "catgoose/nvim-colorizer.lua",
+      event = "FileType *", -- CRITICAL: Load on ANY filetype detection
+      opts = {
+        filetypes = { "*" }, -- Highlight ALL filetypes
+        user_default_options = {
+          RGB = true,
+          RRGGBB = true,
+          names = true,
+          RRGGBBAA = true,
+          rgb_fn = true,
+          hsl_fn = true,
+          css = true,
+          css_fn = true,
+          mode = "background",
+          always_update = false,
+        },
+      },
+    }
 }
 
