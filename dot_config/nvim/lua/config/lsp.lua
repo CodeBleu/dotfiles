eee31289f8ef2d170cd8dbf55d3dc5c4c6ae0f38
Jason Hollis <400979+CodeBleu@users.noreply.github.com>
2026-07-28 11:38:23 -0400

    Updated tmux/nvim configs
        * Updated tmux to allow Undercurls to pass through
        * Fixed deprecated diagnostic commands

--- dot_config/nvim/lua/config/lsp.lua
+++ dot_config/nvim/lua/config/lsp.lua
@@ -22,9 +22,13 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
         vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
 
-        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+        vim.keymap.set("n", "[d", function()
+        vim.diagnostic.jump({ count = 1, float = true })
+        end, opts)
 
-        vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+        vim.keymap.set("n", "]d", function()
+          vim.diagnostic.jump({ count = -1, float = true })
+        end, opts)
     end,
 })
 
